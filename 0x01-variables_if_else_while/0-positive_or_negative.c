71e47001fc818f54305f1bdb135a5a28c1af4001
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-20 13:44:38 -0500

    Erase spaces and generic comments

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,6 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 /**
  * main - Entry point
  *
@@ -15,7 +13,6 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
 if (n > 0)
 printf("%d is positive", n);
 else if (n < 0)
