2d0ff9a1ad20d3251d8de7461cd19c414db41eaf
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-20 14:02:04 -0500

    Fixed  == 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive", n);
-else if (n < 0)
-printf("%d is negative", n);
-else
+else if (n == 0)
 printf("%d is zero", n);
+else
+printf("%d is negative", n);
 return (0);
 }
