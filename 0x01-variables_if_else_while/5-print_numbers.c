de3a5eaee1a2017d94be470b6d5e8b07dcc254d7
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-20 21:14:44 -0500

    Change the printf for putcharar

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
- * main - print the numbers of one digit
+ * main - print te main function
  *
  * Return: 0
  */
 int main(void)
 {
-int num = 0;
-while (num < 10)
-{
-printf("%d\n", num);
-num++;
-}
-return (0);
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
