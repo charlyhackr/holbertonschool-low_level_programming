ea1f3b66c03199fac28527d8c45f678a8d964c31
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-20 23:19:57 -0500

    Add with n2 variable

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,13 +7,16 @@
 int main(void)
 {
 	int n1;
+	int n2;
 	int i;
 
 	for (i = 0; i < 100; i++)
 	{
 		n1 = i / 10;
+		n2 = i / 10;
 
 		putchar(n1 + '0');
+		putchar(n2 + '0');
 
 		if (i < 99)
 		{
