511628ea6639d555ffcde2f3b688c27891af9294
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-20 23:57:48 -0500

    Edit variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,12 @@ int main(void)
 
 	while (f1 <= 8)
 	{
-		second += salto;
-		while (second <= 9)
+		f2 += salto;
+		while (f2 <= 9)
 		{
 			putchar(f1 + '0');
-			putchar(second + '0');
-			if (first != 8)
+			putchar(f2 + '0');
+			if (f1 != 8)
 			{
 				putchar(',');
 				putchar(' ');
