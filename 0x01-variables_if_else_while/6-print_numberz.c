265d454e0e9bed2ff073aeb5c4276f8a2364dba0
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-20 21:29:00 -0500

    Add numberz with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - contains the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
