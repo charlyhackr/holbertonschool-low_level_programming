2c9d72c78273197a39c0598cfe6015578e885d09
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-20 23:55:05 -0500

    Add 100 combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print de combinatios
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int f1 = 0;
+	int f2 = 0;
+	int salto = 1;
+
+	while (f1 <= 8)
+	{
+		second += salto;
+		while (second <= 9)
+		{
+			putchar(f1 + '0');
+			putchar(second + '0');
+			if (first != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			f2++;
+		}
+		f2 = 0;
+		salto++;
+		f1++;
+	}
+	putchar('\n');
+	return (0);
+}
