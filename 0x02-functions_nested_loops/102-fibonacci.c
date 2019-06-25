e6e0c7a14253c40fd7f63960e694c8ef0c1ec4d3
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-25 02:35:39 -0500

    Add fibonnacci series up 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - point entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 1, b = 1, num = 50, c, count;
+
+	printf("The series is \n");
+	printf("%d, %d, ", a, b);
+	count =2;
+	while (count < num)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%d\n", c);
+		count++;
+	}
+}
