60692a595d1a322909708e28dc6da43ff5bbd57d
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-27 13:07:12 -0500

    without putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n >= 0 && n > 10)
+		return (n);
+}
