972ccfeef28b7bb529a579cd2765e8f6d3a0c659
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-27 11:47:37 -0500

    Add 0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Function that checks for uppercase character
+ * @c: int type number
+ *REturn: 1 if uppercase, else return 0
+ */
+int _isupper(int c)
+{
+	while (c > 64 && c < 91)
+		return (1);
+
+	return (0);
+}
