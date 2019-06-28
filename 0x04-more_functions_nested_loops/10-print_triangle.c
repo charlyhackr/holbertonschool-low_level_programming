04da47a31a6516ad2a76eb2024aacf84eddbffb3
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-27 23:07:38 -0500

    Add .h and 10 triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ *
+ * print_triangle - function that prints a triangle, followed by a new line.
+ * @size: Size of triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int space, cont, triangle;
+
+	space = 0;
+	cont = size - 1;
+	while (space < size)
+	{
+		cont = size - 1 -space;
+		triangle = space + 1;
+		while (cont > 0)
+		{
+			_putchar(' ');
+			cont--;
+		}
+		while (triangle > 0)
+		{
+			_putchar('#');
+			triangle--;
+		}
+		_putchar('\n');
+		space++;
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
