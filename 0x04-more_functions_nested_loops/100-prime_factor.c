bae8689046801f0982b3d8eb67dd1c953210cb7e
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-06-27 23:31:48 -0500

    Add factor largest

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	long int num1, num_t, least_factor;
+
+	num1 = 2;
+	least_factor = 2;
+	num_t =  61285247514;
+
+	while (num_t > num1)
+	{
+		if (num_t % num1 == 0)
+		{
+			least_factor = 2;
+			num_t = num_t / num1;
+			num1 = num1 * num1;
+		}
+		else
+		{
+			num1++;
+		}
+	}
+	least_factor = num1;
+	printf("%ld\n", least_factor);
+	return (0);
+}
