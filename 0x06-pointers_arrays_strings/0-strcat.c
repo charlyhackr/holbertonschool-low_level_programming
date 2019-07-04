59efb125ac711fd9915067addb33b9266477f0ca
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-07-04 13:19:28 -0500

    0-string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcat - function that concatenate two string
+ * @dest: char string s1
+ * @src: char string s2
+ * Return: pointer to resulting string to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int st1, st2;
+	for (st1 = 0; dest[st1] != '\0'; st1++)
+		;
+
+	for (st2 = 0; src[st2] != '\0'; st2++)
+	{
+		dest[st1] = src[st2];
+		st1++;
+	}
+	dest[st1] = '\0';
+	return (dest);
+}
