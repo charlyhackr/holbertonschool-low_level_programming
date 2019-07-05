289f3094886502f98d6965ee14a7f82c30b501e4
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-07-04 22:56:34 -0500

    add last  root 13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * rot13 - rotate characteres 13 places in the alphabet
+ * @s: char string
+ * Return: string s rotated
+ */
+
+char *rot13(char *s)
+{
+	int cont;
+	char codemay[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char codemin[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (cont = 0; s[cont] != '\0'; cont++)
+	{
+		if ((s[cont] > 64 && s[cont] < 91) || (s[cont] > 96 && s[cont] < 123))
+		{
+			s[cont] = (s[cont] - 65 > 25) ?
+				codemin[s[cont] - 97] : codemay[s[cont] - 65];
+		}
+	}
+	return (s);
+}
