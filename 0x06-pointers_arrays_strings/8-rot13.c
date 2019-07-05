138b725967f35caca08ae65b39597103b5ace3ad
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-07-04 23:15:03 -0500

    other rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- * rot13 - that encodes a string using rot13.
- * @s: string to convert to rot13
- * Return: return converted string
+ * rot13 - conver string o rot13
+ * @s: string
+ * Return: string
  */
+
 char *rot13(char *s)
 {
 	int i;
@@ -12,16 +13,16 @@ char *rot13(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-		       (s[i] >= 'A' && s[i] <= 'Z'))
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 		{
-			if ((s[i] >= 'N' && s[i] <= 'Z') ||
-			    (s[i] >= 'n' && s[i] <= 'z'))
-				s[i] -= 13;
-			else
-				s[i] += 13;
-			i++;
+			s[i] = (s[i] + 13);
 		}
+		else
+			while ((s[i] >= 'n' && s[i] <= 'z') ||
+			      (s[i] >= 'N' && s[i] <= 'Z'))
+			{
+				s[i] = (s[i] - 13);
+			}
 		i++;
 	}
 	return (s);
