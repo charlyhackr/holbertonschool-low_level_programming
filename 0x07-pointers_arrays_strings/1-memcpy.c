9aebc42a992c10a42ddf3a4f7977c57cefd6f17d
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-07-08 18:27:53 -0500

    Add 1 memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _mencpy - copy memory area
+ * @dest: char array to copy into
+ * @src: char array to copy from
+ * @n: number of elements to copy
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *punt;
+
+	punt = dest;
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	return (punt);
+}
