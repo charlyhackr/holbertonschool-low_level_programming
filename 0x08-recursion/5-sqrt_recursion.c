2cdd039c8ad1f8f00f770dc52c55482ebcf13f06
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-07-10 19:20:47 -0500

    Add sqrt_root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - blank
+ * @n: natural number
+ * Return: -1 if no natural square root, else return sqaure root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (aux (n, 1));
+}
+
+/**
+ * aux -  function to solve sqrt_recursion
+ * @sr: number to determine square root
+ * @comp: compare against sr
+ * Return: sqaure root if natural sqaure root, or -1 if none found
+ */
+
+int aux(int sr, int comp)
+{
+	int raiz;
+
+	raiz = comp * comp;
+	if (raiz == sr)
+		return (comp);
+	else if (raiz < sr)
+		return (aux(sr, comp + 1));
+	else
+		return (-1);
+}
