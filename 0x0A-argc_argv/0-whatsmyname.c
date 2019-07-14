f9018da847ef4b09679187f2ac7ef32e5b360c71
charlyhackr <930@holbertonschool.comgit config --global user.name charlyhackrqxitexit[3~5~[3~ exitgit exitgit git add 0-hello_worldgit pushqgit config --global user.email 930@holbertonschool.com>
2019-07-14 16:00:45 -0500

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main -  rints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: Always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
