2c6c155912a6f65995a61a8de36d28b7be4f178c
charlyhackr <930@holbertonschool.com>
2019-07-18 18:47:11 -0500

    Add 1-string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,58 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenate 2 strings, only n bytes
- * @s1: string 1
- * @s2: string 2
- * @n: bytes to include of s2
- * Return: NULL if fails, else pointer to malloc memory
+ * _strlen - string length
+ * @str: string
+ * Return: length string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int con = 0;
+
+	while (str[con] != 0)
+		con++;
+	return (con);
+}
+
+/**
+ * *string_nconcat - string concatenation
+ * @s1: char 1
+ * @s2: char 2
+ * @n: number
+ * Return: new string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *pointer;
-	int strlen2, a, con;
+	char *newstrn;
+	unsigned int strlen_s1;
+	unsigned int strlen_s2;
+	unsigned int _malloc, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	strlen2 = (unsigned int)_strlen(s1);
-	pointer = malloc((strlen2 + n + 1) * sizeof(char));
-	if (pointer == NULL)
+	strlen_s1 = _strlen(s1);
+	strlen_s2 = _strlen(s2);
+
+	if (n >= strlen_s2)
+		_malloc = strlen_s1 + strlen_s2;
+	else
+		_malloc = strlen_s1 + n;
+
+	newstrn = malloc(sizeof(char) * (_malloc + 1));
+	if (newstrn == NULL)
 		return (NULL);
-	for (a = 0, con = 0; a < (strlen2 + n); a++)
+	for (i = 0; i < _malloc; i++)
 	{
-		if (a < strlen2)
-			pointer[a] = s1[a];
+		if (i < strlen_s1)
+			newstrn[i] = s1[i];
 		else
-			pointer[a] = s2[con++];
+			newstrn[i] = s2[i - strlen_s1];
 	}
-	pointer[a] = '\0';
-
-	return (pointer);
-}
-
-/**
- * _strlen - find length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	int a;
-	for (a = 0; s[a] != '\0'; a++)
-		;
-	return (a);
+	newstrn[i] = '\0';
+	return (newstrn);
 }
