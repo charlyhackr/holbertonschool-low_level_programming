3ca34ef2185da634793605a0d09ef2e9a98e228a
charlyhackr <930@holbertonschool.com>
2019-07-22 14:36:16 -0500

    Fixed PArenthesis Add absolute valor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef 3_FUNCTION_LIKE_MAKRO_H_
-#define 3_FUNCTION_LIKE_MAKRO_H_
-#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#ifndef TH_FUNCTION_LIKE_MAKRO_H_
+#define TH_FUNCTION_LIKE_MAKRO_H_
+#define ABS(x) (((x) > 0) ? (x) : (x) * -1)
 #endif /* 3_FUNCTION_LIKE_MAKRO_H_ */
