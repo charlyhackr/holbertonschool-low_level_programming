0a56f9a9601fe53b752f102f8fc34242c86fdc13
charlyhackr <930@holbertonschool.com>
2019-07-22 21:11:11 -0500

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new structure of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_o;
+
+#endif
