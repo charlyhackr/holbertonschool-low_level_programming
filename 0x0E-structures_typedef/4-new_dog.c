03d63bef16e59f4ae0a4dd786c9e1c0fd3ca3e1b
charlyhackr <930@holbertonschool.com>
2019-07-22 22:28:54 -0500

    Add 4 and dog.h with dog_t

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *dogn;
+	int ai, je, ki;
+	char *n, *o;
+
+	dogn = malloc(sizeof(struct dog));
+	if (dogn == NULL)
+		return (NULL);
+
+	for (ai = 0; name[ai] != '\0'; ai++)
+		;
+	for (je = 0; owner[je] != '\0'; je++)
+		;
+
+	n = malloc(sizeof(char) * ai + 1);
+	if (n == NULL)
+	{
+		free(dogn);
+		return (NULL);
+	}
+	o = malloc(sizeof(char) * je + 1);
+	if (o == NULL)
+	{
+		free(dogn);
+		free(dogn);
+		return (NULL);
+	}
+	for (ki = 0; ki <= ai; ki++)
+		n[ki] = name[k];
+	for (ki = 0; ki <= je; ki++)
+		o[ki] = owner[ki];
+
+	dogn->name = n;
+	dogn->age = age;
+	dogn->owner = o;
+
+	return (dogn);
+}
