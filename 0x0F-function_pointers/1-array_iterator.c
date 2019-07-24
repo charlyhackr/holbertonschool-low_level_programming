20b36e2cf51965eebeb141e75cff1dc8ce4ab4d2
charlyhackr <930@holbertonschool.com>
2019-07-24 16:15:40 -0500

    FIxed bollean operator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,9 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	size_t cont;
 
-	if (array == NULL || action == NULL)
-		exit(98);
-	for (cont = 0; cont < size; cont++)
+	if (array != NULL && action != NULL)
+	{
+		for (cont = 0; cont < size; cont++)
 		action(array[cont]);
+	}
 }
