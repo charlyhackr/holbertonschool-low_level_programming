c25754df0edcccf2504325b97348bb1c123c419f
charlyhackr <930@holbertonschool.com>
2019-07-24 16:10:23 -0500

    Add NUll validation for array and action pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	size_t cont;
+
+	if (array == NULL || action == NULL)
+		exit(98);
 	
 	for (cont = 0; cont < size; cont++)
 		action(array[cont]);
