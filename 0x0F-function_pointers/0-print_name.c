2bb8603a2644f58b38929281f86c572ea0afb781
charlyhackr <930@holbertonschool.com>
2019-07-24 15:24:31 -0500

    Add print .c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
