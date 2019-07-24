a97d21fb0acb3176f21926329128be550910fb64
charlyhackr <930@holbertonschool.com>
2019-07-24 16:05:07 -0500

    Fixed add .h at bottom of file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,4 @@
-#include "function_pointers"
+#include "function_pointers.h"
 
 /**
  * array_iterator -function given as a parameter on each element of an array.
@@ -10,7 +10,7 @@
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	size_t cont;
-
+	
 	for (cont = 0; cont < size; cont++)
 		action(array[cont]);
 }
