1c48b5027fc5d33c0421d2b09a9c1bdc5fec6455
charlyhackr <930@holbertonschool.com>
2019-07-24 16:12:28 -0500

    FIxed trailing whitespace

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,6 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 
 	if (array == NULL || action == NULL)
 		exit(98);
-	
 	for (cont = 0; cont < size; cont++)
 		action(array[cont]);
 }
