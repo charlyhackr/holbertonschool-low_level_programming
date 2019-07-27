9be44e5fd02f10e086233dc2be14a39c2a62a6dd
charlyhackr <930@holbertonschool.com>
2019-07-27 15:15:15 -0500

    Add struct form_t

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,9 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct arr_fm
+{
+	char *arr_fm;
+	void (*p)();
+} form_t;
 #endif
