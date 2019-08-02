08597f670d77593181ff516a91fd3f7407675ee1
charlyhackr <930@holbertonschool.com>
2019-08-01 20:42:37 -0500

    Fix comments in lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,11 +2,20 @@
 #define LISTS_H_
 #include <stddef.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc strings)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description:  singly linked list node structure
+ */
+
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
