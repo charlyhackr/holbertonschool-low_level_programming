e38879be7edd3d79632ffa1e85e7100010810e33
charlyhackr <930@holbertonschool.com>
2019-08-01 20:45:25 -0500

    Fix the prohited spaces

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,20 @@
-#include "lists.h"
+|#include "lists.h"
 #include <stdio.h>
 
 /**
  * print_list -prints all the elements of a list_t list.
  * @h: head of linked list
+ * Return counter
  */
 
-size_t print_list (const list_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t cont = 0;
 
 	while (h != NULL)
 	{
-		if(h->str == NULL)
-		{
+		if (h->str == NULL)
+		
 			printf("[0] %p\n", h->str);
 			h = h->next;
 			cont++;
