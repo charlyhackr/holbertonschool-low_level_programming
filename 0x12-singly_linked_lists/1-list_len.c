1374abf6812a9a3ce05c58146bc080903f53e136
charlyhackr <930@holbertonschool.com>
2019-08-01 21:34:04 -0500

    Fix list function description

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_list -prints all the elements of a list_t list.
+ * list_len - returns the number of elements in a linked list_t list.
  * @h: head of linked list
  * Return: counter
  */
