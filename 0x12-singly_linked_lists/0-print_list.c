d8faa2691aabf03013d63d6ea26d4bb56aa15b7f
charlyhackr <930@holbertonschool.com>
2019-08-01 20:30:33 -0500

    Add 0-print.lists.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list -prints all the elements of a list_t list.
+ * @h: head of linked list
+ */
+
+size_t print_list (const list_t *h)
+{
+	size_t cont = 0;
+
+	while (h != NULL)
+	{
+		if(h->str == NULL)
+		{
+			printf("[0] %p\n", h->str);
+			h = h->next;
+			cont++;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			cont++;
+		}
+	}
+
+	return (cont);
+}
