04a12698eedd1a1f42afbe8663eb49cecef1e0a9
charlyhackr <930@holbertonschool.com>
2019-08-08 21:14:00 -0500

    Add file binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/*
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: return to the sum or 0 if there an un unconvertable char
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int suma, potencia;
+	int pos;
+
+	if (b == NULL)
+		return (0);
+
+	for (pos = 0; b[pos]; pos++)
+	{
+		if (b[pos] != '0' && b[pos] != '1')
+			return (0);
+	}
+	for (potencia = 1, suma = 0, pos--; pos >= 0; pos--, potencia *= 2)
+	{
+		if (b[pos] == '1')
+			suma += potencia;
+	}
+	return (suma);
+}
