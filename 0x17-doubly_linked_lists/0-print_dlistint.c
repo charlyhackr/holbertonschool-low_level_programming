f1ec1700cc00e30c33f12190e057573c37449b5f
charlyhackr <930@holbertonschool.com>
2019-09-14 21:25:42 -0500

    Add print lisitint

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print all the values of each nodes in a list
+ * @h: head pointer in a doubly linked list
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *nAux;
+	size_t conta;
+
+	nAux = h;
+	while (nAux && nAux -> prev)
+		nAux = nAux -> prev;
+
+	for (count = 0; nAux; conta++, nAux = nAux -> next)
+		printf("%d\n", nAux -> n);
+
+	return (conta);
