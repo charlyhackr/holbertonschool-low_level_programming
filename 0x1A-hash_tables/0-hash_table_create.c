62c61fa5131abdcc82c7165edbd460cfb8f9d1c8
charlyhackr <930@holbertonschool.com>
2019-10-11 14:09:28 -0500

    add create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int a;
+
+	ht = malloc(sizeof(hash_table_t));
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (a = 0; a < size; a++)
+		ht->array[a] = NULL;
+	return (ht);
+}
