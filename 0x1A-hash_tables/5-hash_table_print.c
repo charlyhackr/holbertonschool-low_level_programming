286095a94fc4b43c65dd291bf44758dfad81043f
charlyhackr <930@holbertonschool.com>
2019-10-11 18:41:19 -0500

    add  print  values

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print a hash table
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int a;
+	int pal;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (a = 0, pal = 0; a < ht->size; a++)
+	{
+		temp = ht->array[a];
+		if (temp != NULL)
+		{
+			if (pal == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			while ((temp = temp->next) != NULL)
+			{
+				printf(", ");
+				printf("'%s':, '%s'", temp->key, temp->value);
+			}
+
+			pal = 1;
+		}
+	}
+
+	printf("}\n");
+}
