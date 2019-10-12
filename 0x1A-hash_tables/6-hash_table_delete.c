fd22d26dbffd86824c552efcdeb526d1c089e532
charlyhackr <930@holbertonschool.com>
2019-10-11 20:12:50 -0500

    delete hash function

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table given
+ * Return: 0
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp, *anter;
+	unsigned long int siz, a;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return;
+	}
+
+	for (a = 0, siz = ht->size; a < siz; a++)
+	{
+		temp = ht->array[a];
+		while (temp)
+		{
+			anter = temp;
+			temp = temp->next;
+			free(anter->key);
+			free(anter->value);
+			free(anter);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
